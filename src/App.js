aaf38d4e9a1ce41b5090c0c92f2a677ff832957f
caminobelllo <alstpgml0812@gmail.com>
2023-05-16 03:37:29 +0900

    Modify Route path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           path={`${process.env.PUBLIC_URL}/movie/:id`}
           element={<Detail />}
         ></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
